466d7c2ba2e4c97b170080662f7e5330fc741bcf
Kevin Renner <renke@wowway.com>
2015-09-27 05:45:36 -0500

    Tried to make it work, or something

--- budget.js
+++ budget.js
@@ -1,5 +1,5 @@
-Expenses = Meteor.Collection('expenses');
-Reveneues = Meteor.Collection('revenues');
+Expenses = new Meteor.Collection('expenses');
+Revenues = new Meteor.Collection('revenues');
 
 
 Router.route('/login');
@@ -11,10 +11,10 @@ Router.route('/create');
 Router.route('/view');
 Router.route('/home');
 Router.route('/logout');
-if (Meteor.isClient){
-  Template.login.onCreated(function(){
-    console.log("The 'login' template was just created.");
-});
+if (Meteor.isClient) {
+    Template.login.onCreated(function () {
+        console.log("The 'login' template was just created.");
+    });
 
     var h_expenses = Meteor.subscribe('expenses');
     var h_revenues = Meteor.subscribe('revenues');
@@ -25,121 +25,135 @@ if (Meteor.isClient){
     var expCategory;
     var revCategory;
 
-    Tracker.autorun(function() {
-        if(h_expenses.ready()) {
+    var ExpChartData;
+    var RevChartData;
+    var PieChart;
+
+    Tracker.autorun(function () {
+        if (h_expenses.ready()) {
             if (expCategory == "")
                 expData = Expenses.find().fetch();
             else
-                expData = Expenses.find({"id":expCategory}).fetch();
+                expData = Expenses.find({"id": expCategory}).fetch();
+            ExpChartData = expData.map(function (data) {
+                return {label: data.Category, value: data.Amount};
+            });
         }
-        if(h_revenues.ready()) {
+        if (h_revenues.ready()) {
             if (revCategory == "")
-                revData = Expenses.find().fetch();
+                revData = Revenues.find().fetch();
             else
-                revData = Expenses.find({"id":revCategory}).fetch();
+                revData = Revenues.find({"id": revCategory}).fetch();
+            RevChartData = revData.map(function (data) {
+                return {name: data.Category, y: data.Amount};
+            });
+
+            var canvas = $("#chartCanvasId")[0];
+            var context = canvas.getContext("2d");
+            PieChart = new Chart(context).Pie(RevChartData);
         }
     })
 
-Template.login.onRendered(function(){
-    $('.login').validate({
-        submitHandler: function(event){
-            var email = $('[name=email]').val();
-            var password = $('[name=password]').val();
-            Meteor.loginWithPassword(email, password, function(error){
-                if(error){
-                    if(error.reason == "User not found"){
-                        validator.showErrors({
-                            email: error.reason    
-                        });
+    Template.login.onRendered(function () {
+        $('.login').validate({
+            submitHandler: function (event) {
+                var email = $('[name=email]').val();
+                var password = $('[name=password]').val();
+                Meteor.loginWithPassword(email, password, function (error) {
+                    if (error) {
+                        if (error.reason == "User not found") {
+                            validator.showErrors({
+                                email: error.reason
+                            });
+                        }
+                        if (error.reason == "Incorrect password") {
+                            validator.showErrors({
+                                password: error.reason
+                            });
+                        }
+                    } else {
+                        var currentRoute = Router.current().route.getName();
+                        if (currentRoute == "login") {
+                            Router.go("home");
+                        }
                     }
-                    if(error.reason == "Incorrect password"){
-                        validator.showErrors({
-                            password: error.reason    
-                        });
-                    }
-                } else {
-                    var currentRoute = Router.current().route.getName();
-                    if(currentRoute == "login"){
+                });
+            }
+        });
+    });
+
+    Template.register.onRendered(function () {
+        $('.register').validate({
+            submitHandler: function (event) {
+                var email = $('[name=email]').val();
+                var password = $('[name=password]').val();
+                Accounts.createUser({
+                    email: email,
+                    password: password
+                }, function (error) {
+                    if (error) {
+                        if (error.reason == "Email already exists.") {
+                            validator.showErrors({
+                                email: "That email already belongs to a registered user."
+                            });
+                        }
+                    } else {
                         Router.go("home");
                     }
-                }
-            });
-        }
+                });
+            }
+        });
     });
-});
 
-Template.register.onRendered(function(){
-    $('.register').validate({
-        submitHandler: function(event){
+    Template.login.onDestroyed(function () {
+        console.log("The 'login' template was just destroyed.");
+    });
+    Template.register.events({
+        'submit': function (event) {
+            console.log("line 4");
+            event.preventDefault();
             var email = $('[name=email]').val();
             var password = $('[name=password]').val();
+
             Accounts.createUser({
                 email: email,
                 password: password
-            }, function(error){
-              if(error){
-                  if(error.reason == "Email already exists."){
-                      validator.showErrors({
-                          email: "That email already belongs to a registered user."   
-                      });
-                  }
-              }else {
+            })
+            Router.go("home");
+        }
+
+    });
+    Template.login.events({
+        'submit': function (event) {
+            event.preventDefault();
+
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            Meteor.loginWithPassword(email, password, function (error) {
+                if (error) {
+                    console.log(error.reason);
+                }
+                else {
                     Router.go("home");
                 }
             });
-        }    
+
+
+        }
     });
-});
 
-Template.login.onDestroyed(function(){
-    console.log("The 'login' template was just destroyed.");
-});
-  console.log("line 1");
-  Template.register.events({
-      'submit': function(event){
-        console.log("line 4");
-          event.preventDefault();
-          var email = $('[name=email]').val();
-          var password = $('[name=password]').val();
-
-          Accounts.createUser({
-            email: email,
-            password: password
-          })
-          Router.go("home");
-      }
-      
-  });
-  console.log("line 20");
-  Template.login.events({
-    'submit': function(event){
-        event.preventDefault();
-        
-        var email = $('[name=email]').val();
-        var password = $('[name=password]').val();
-        Meteor.loginWithPassword(email, password,function(error){
-          if (error){
-            console.log(error.reason);
-          }
-          else{
-            Router.go("home");
-          }
-        });
-        
-
-
+    Template.containers.rendered = function () {
+        window.piechart = PieChart;
+    };
+}
+    if (Meteor.isServer) {
+        Meteor.startup(function () {
+            Meteor.publish('expenses', function () {
+                return Expenses.find();
+            });
+            Meteor.publish('revenues', function () {
+                return Revenues.find();
+            });
+        })
     }
-});
 
-if(Meteor.isServer) {
-    Meteor.startup(function() {
-        Meteor.publish('expenses', function() {
-            return Expenses.find();
-        });
-        Meteor.publish('revenues', function() {
-            return Expenses.find();
-        });
-    })
-}
-
-}
